e991253b9ab6460693686e2351b6b2523e9903a3
llattanzi <lauren71442@gmail.com>
2022-02-20 20:28:04 -0500

    Add prompt for player name and fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
