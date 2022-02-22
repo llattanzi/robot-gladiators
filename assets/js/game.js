7d4843ec46573a2bc3fd8709d9ddd0fbb5c48c3e
llattanzi <lauren71442@gmail.com>
2022-02-22 16:55:45 -0500

    Fix player-name bug to not allow empty or null values

--- assets/js/game.js
+++ assets/js/game.js
@@ -171,8 +171,18 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+//  function to set name
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    Name: window.prompt("What is your robot's name?"),
+    Name: getPlayerName(),
     Health: 100,
     Attack: 10,
     Money: 10,
